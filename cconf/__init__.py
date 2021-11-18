e27572c743bbfa4d25a1b0af9ae613dcaaff3549
Dan Watson <dcwatson@gmail.com>
2021-11-18 14:32:21 -0500

    Drop version number for preview release

--- cconf/__init__.py
+++ cconf/__init__.py
@@ -14,7 +14,7 @@ from .base import (
 from .dburl import register as register_database
 from .types import CommaSeparatedStrings, DatabaseDict, Duration, Secret
 
-__version__ = "1.0.0"
+__version__ = "0.9.0"
 __version_info__ = tuple(int(num) for num in __version__.split("."))
 
 __all__ = [
